a6b8003ca119f7e2235a6ab01e5effe82c4f602a
Nadya Djojosantoso <hello@nadyafebi.com>
2021-02-01 18:53:55 -0800

    Make agent class, add invincible and summon ghast functions

--- project/GhastTennis.py
+++ project/GhastTennis.py
@@ -4,67 +4,82 @@ import MalmoPython
 import os
 import sys
 import time
+import functools
+print = functools.partial(print, flush=True)
 
-if sys.version_info[0] == 2:
-    sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)  # flush print output immediately
-else:
-    import functools
-    print = functools.partial(print, flush=True)
+class Agent:
+    def __init__(self, host):
+        self.host = host
 
-# Create default Malmo objects:
-agent_host = MalmoPython.AgentHost()
-try:
-    agent_host.parse( sys.argv )
-except RuntimeError as e:
-    print('ERROR:',e)
-    print(agent_host.getUsage())
-    exit(1)
-if agent_host.receivedArgument("help"):
-    print(agent_host.getUsage())
-    exit(0)
+    def start(self, mission, mission_record):
+        # Start mission
+        try:
+            self.host.startMission(mission, mission_record)
+        except RuntimeError as e:
+            print('Error starting mission', e)
 
-# Mission setup:
-mission_file = './mission.xml'
-with open(mission_file, 'r') as f:
-    print("Loading mission from %s" % mission_file)
-    mission_xml = f.read()
-    my_mission = MalmoPython.MissionSpec(mission_xml, True)
-my_mission_record = MalmoPython.MissionRecordSpec()
+        # Wait for mission to start
+        print("Waiting for the mission to start ", end=' ')
+        world_state = self.host.getWorldState()
+        while not world_state.has_mission_begun:
+            print(".", end="")
+            time.sleep(0.1)
+            world_state = self.host.getWorldState()
+            for error in world_state.errors:
+                print("Error:",error.text)
 
-# Attempt to start a mission:
-max_retries = 3
-for retry in range(max_retries):
+        # Initialize mission
+        print()
+        print("Mission running ", end=' ')
+        self.initialize()
+
+        # Take action
+        while world_state.is_mission_running:
+            print(".", end="")
+            time.sleep(0.1)
+            world_state = agent_host.getWorldState()
+            self.takeAction(world_state)
+            for error in world_state.errors:
+                print("Error:",error.text)
+
+        # End mission
+        print()
+        print("Mission ended")
+
+    def initialize(self):
+        self.makeInvincible()
+        self.summonGhast(0, 10, 0)
+
+    def takeAction(self, world_state):
+        pass
+
+    def makeInvincible(self):
+        self.host.sendCommand('chat /effect @p 11 10000 255 True')
+
+    def summonGhast(self, x, y, z):
+        self.host.sendCommand(f'chat /summon Ghast {x} {y} {z}')
+
+if __name__ == '__main__':
+    # Create default Malmo objects
+    agent_host = MalmoPython.AgentHost()
     try:
-        agent_host.startMission( my_mission, my_mission_record )
-        break
+        agent_host.parse(sys.argv)
     except RuntimeError as e:
-        if retry == max_retries - 1:
-            print("Error starting mission:",e)
-            exit(1)
-        else:
-            time.sleep(2)
+        print('ERROR:',e)
+        print(agent_host.getUsage())
+        exit(1)
+    if agent_host.receivedArgument("help"):
+        print(agent_host.getUsage())
+        exit(0)
 
-# Loop until mission starts:
-print("Waiting for the mission to start ", end=' ')
-world_state = agent_host.getWorldState()
-while not world_state.has_mission_begun:
-    print(".", end="")
-    time.sleep(0.1)
-    world_state = agent_host.getWorldState()
-    for error in world_state.errors:
-        print("Error:",error.text)
+    # Setup mission
+    mission_file = './mission.xml'
+    with open(mission_file, 'r') as f:
+        print("Loading mission from %s" % mission_file)
+        mission_xml = f.read()
+        mission = MalmoPython.MissionSpec(mission_xml, True)
+    mission_record = MalmoPython.MissionRecordSpec()
 
-print()
-print("Mission running ", end=' ')
-
-# Loop until mission ends:
-while world_state.is_mission_running:
-    print(".", end="")
-    time.sleep(0.1)
-    world_state = agent_host.getWorldState()
-    for error in world_state.errors:
-        print("Error:",error.text)
-
-print()
-print("Mission ended")
-# Mission has ended.
+    # Create agent
+    agent = Agent(agent_host)
+    agent.start(mission, mission_record)
